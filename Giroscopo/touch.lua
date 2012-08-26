5308b22668db9b53a5bc17cc3516f53662841f21
Maximiliano Chiesa <maxi.chiesa@gmail.com>
2012-08-26 19:51:41 -0300

    Versión 2
    giroscopo

--- Giroscopo/touch.lua
+++ Giroscopo/touch.lua
@@ -2,10 +2,8 @@ local im = getInputManager()
 local scene = getCurrentScene()
 local viewport = Viewport(scene:getObjectByName("viewport"))
 local window = RenderWindow(scene:getMainView())
-local giroscopo_01 = Scenette(scene:getObjectByName("giroscopo_01"))
-local giroscopo_02 = Scenette(scene:getObjectByName("giroscopo_02"))
-local giroscopo_04 = Scenette(scene:getObjectByName("giroscopo_04"))
 local touchscreen = nil
+local touched = false
 local mouse = nil
 
 if isTouchScreenDeviceAvailable() then
@@ -22,11 +20,15 @@ repeat
 		local obj = nil
 		
 		if touchscreen then
-		
 			local touches = touchscreen:getTouches()
 			if (#touches > 0) then
-				local t = touches[1]
-				obj = viewport:pick(t.position.x, t.position.y)
+				if not touched then
+					local t = touches[1]
+					obj = viewport:pick(t.position.x, t.position.y)
+				end
+				touched = true
+			else
+				touched = false
 			end
 			
 		else
@@ -39,46 +41,24 @@ repeat
 		end
 		
 		if obj and not obj:isNull() then
-			if obj:getName() == "boton_01" then
-				if not giroscopo_01:getVisible() then
-					giroscopo_01:setVisible(true)
-				end
-				if giroscopo_02:getVisible() then
-					giroscopo_02:setVisible(false)
-				end
-				if giroscopo_04:getVisible() then
-					giroscopo_04:setVisible(false)
-				end
-			elseif obj:getName() == "boton_02" then
-				if not giroscopo_02:getVisible() then
-					giroscopo_02:setVisible(true)
-				end
-				if giroscopo_01:getVisible() then
-					giroscopo_01:setVisible(false)
-				end
-				if giroscopo_04:getVisible() then
-					giroscopo_04:setVisible(false)
-				end
-				local animation = giroscopo_02:getAnimation(0)
-				if not animation:isPlaying() then
-					animation:play(0)
-					animation:setLoop(true)
-				end
-			elseif obj:getName() == "boton_04" then
-				if not giroscopo_04:getVisible() then
-					giroscopo_04:setVisible(true)
-				end
-				if giroscopo_01:getVisible() then
-					giroscopo_01:setVisible(false)
-				end
-				if giroscopo_02:getVisible() then
-					giroscopo_02:setVisible(false)
-				end
-				local animation = giroscopo_04:getAnimation(0)
-				if not animation:isPlaying() then
-					animation:play(0)
-					animation:setLoop(true)
-				end				
+			if obj:getName() == 'boton_01' then
+				load_model(1)
+			elseif obj:getName() == 'boton_02' then
+				load_model(2)
+			elseif obj:getName() == 'boton_04' then
+				load_model(4)
+			elseif obj:getName() == 'boton_animaciones' then
+				toggle_animations()
+			elseif obj:getName() == 'boton_vectores' then
+				toggle_vectors()
+			elseif obj:getName() == 'boton_velocidadangular' then
+				toggle_vector_group('velocidad_angular')
+			elseif obj:getName() == 'boton_momentoangular' then
+				toggle_vector_group('momento_angular')
+			elseif obj:getName() == 'boton_torque' then
+				toggle_vector_group('torque')
+			elseif obj:getName() == 'boton_fuerzas' then
+				toggle_vector_group('fuerzas')
 			end
 		end
 		
