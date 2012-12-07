91daef210e1cfdbc1b7f14a4afdf8a809e024476
Martin Dutra <martindsq@hotmail.com>
2012-12-07 21:51:16 -0200

    jhgkjhg

--- Giroscopo-eclipse/src/com/educar/giroscopo/EducARActivity.java
+++ Giroscopo-eclipse/src/com/educar/giroscopo/EducARActivity.java
@@ -3,15 +3,20 @@ package com.educar.giroscopo;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import org.apache.http.util.ByteArrayBuffer;
 import org.json.JSONArray;
 
 import ti.dfusionmobile.tiComponent;
@@ -20,6 +25,7 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,8 +35,6 @@ import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.educar.giroscopo.notas.constants.NotasConstants;
-import com.educar.giroscopo.notas.handlers.NotasHandler;
-import com.educar.giroscopo.notas.handlers.SharedPreferencesHandler;
 import com.educar.giroscopo.utils.HttpUtil;
 
 public class EducARActivity extends Activity {
@@ -126,8 +130,11 @@ public class EducARActivity extends Activity {
 		}
 	}
 	
+	private File getDownloadDir(){
+		return getDir("tmp", Context.MODE_PRIVATE);
+	}
+	
 	private String getZipFilename(){
-		//return "content.zip";
 		return "dfusion.zip";
 	}
 	
@@ -135,8 +142,16 @@ public class EducARActivity extends Activity {
 		return getDir("www", Context.MODE_PRIVATE);
 	}
 	
-	private File getProjectFile(){
-		return new File(getUnzipDir(), "Giroscopo.dpd");
+	private String getProjectFilename(){
+		return "Giroscopo.dpd";
+	}
+	
+	private DateFormat getDateFormat(){
+		return new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+	}
+	
+	private SharedPreferences getSharedPreferences(){
+		return this.getSharedPreferences("", Activity.MODE_PRIVATE);
 	}
 	
 	class UnzipProjectTask extends AsyncTask<Void, Void, Void> {
@@ -156,61 +171,21 @@ public class EducARActivity extends Activity {
 		
 		@Override
 		protected Void doInBackground(Void... params) {
+			File projectFile = new File(getUnzipDir(), getProjectFilename());
 			
-			downloadProject();
-			
-			File projectFile = getProjectFile();
-			
-			if(!projectFile.exists()){
-				
-				File dir = getUnzipDir();
+			Date date = new Date();
+			if(!projectFile.exists() || shouldDownloadProject(date)){
 				InputStream in = null;
 				try {
-					in = getAssets().open(getZipFilename(), Context.MODE_PRIVATE);
+					// in = getAssets().open(getZipFilename(), Context.MODE_PRIVATE);
+					File file = new File(getDownloadDir(), getZipFilename());  
+					in = downloadProject(file, date);
 					
-					ZipInputStream zis = null;
-					try {
-						zis = new ZipInputStream(new BufferedInputStream(in));
-						
-						ZipEntry ze;
-						while ((ze = zis.getNextEntry()) != null) {
-							if(ze.isDirectory()){
-								File file = new File(dir, ze.getName());
-								file.mkdirs();
-							} else {
-								File file = new File(dir, ze.getName());
-								
-								OutputStream out = null;
-								try {
-									out = new BufferedOutputStream(new FileOutputStream(file), 1024);
-									byte[] buffer = new byte[1024];
-									int count;
-									while ((count = zis.read(buffer)) != -1) {
-										out.write(buffer, 0, count);
-									}
-								} catch (IOException e){
-									Log.d("EducARActivity", e.getMessage());
-									error = true;
-								} finally {
-									if(out != null){
-										try {
-											out.flush();
-											out.close();
-										} catch (IOException e){ }
-									}
-								}
-							}
-						}
-					} catch (IOException e){
-						Log.d("EducARActivity", e.getMessage());
-						error = true;
-					} finally {
-						if(zis != null){
-							try {
-								zis.close();
-							} catch (IOException e) { }
-						}
-					}
+					Log.d("EducARActivity", "Emptying " + getUnzipDir().getPath());
+					emptyDirectory(getUnzipDir());
+					
+					Log.d("EducARActivity", "Extracting " + file.getPath());
+					extractZip(in);
 				} catch (IOException e) {
 					Log.d("EducARActivity", e.getMessage());
 					error = true;
@@ -220,45 +195,129 @@ public class EducARActivity extends Activity {
 							in.close();
 						} catch (IOException e){ }
 					}
-				}
-			}
+				}				
+			} 
+			
 			return null;
 		}
 		
-		private boolean downloadProject(){
+		private boolean shouldDownloadProject(Date date){
+			String lastDownloaded = getSharedPreferences().getString(NotasConstants.ULTIMA_NOTA_BAJADA, "never");
+			Log.d("EducARActivity", "Project last downloaded on " + lastDownloaded);
 			
-			SharedPreferencesHandler preferences = new SharedPreferencesHandler(EducARActivity.this);
-			String lastDateDownloaded = preferences.getValue(NotasConstants.ULTIMA_NOTA_BAJADA);
-			NotasHandler notasHandler = new NotasHandler(EducARActivity.this);
-			
-			if(lastDateDownloaded == null || lastDateDownloaded.equals("")){
-				return notasHandler.downloadProject(getUnzipDir());
+			if(lastDownloaded.equals("never")){
+				return false;
+			} else {
+				String url = NotasConstants.PROJECT_LAST_MODIFIED;
+				Log.d("EducARActivity", "Downloading JSON from " + url);
 				
-				//Falta setear en las shared preferences la fecha de actualizacion
-				
-				
-			}else{
-				JSONArray projectLastModified = HttpUtil.getRequest(NotasConstants.PROJECT_LAST_MODIFIED);
+				JSONArray json = HttpUtil.getRequest(url);
+				try {
+					String lastModified = json.getJSONObject(0).getString("last_modified_date");
+					Log.d("EducARActivity", "Project last modified on " + lastModified);
 					
-				try{
-					String sProjectLastModified = projectLastModified.getJSONObject(0).getString("last_modified_date");
+					Date dateLastModified = getDateFormat().parse(lastModified);
+					Date dateLastDownloaded = getDateFormat().parse(lastDownloaded);
 					
-					SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-					Date oProjectLastModified = df.parse(sProjectLastModified);
-					Date oLastDateDownloaded = df.parse(lastDateDownloaded);
-					
-					if(oProjectLastModified.after(oLastDateDownloaded)){
-						return notasHandler.downloadProject(getUnzipDir());
+					if(dateLastModified.after(dateLastDownloaded)){	
+						date.setTime(dateLastModified.getTime());
+						return true;
+					} else {
+						return false;
 					}
-					
-					
-				}catch(Exception e){
+				} catch(Exception e) {
 					return false;
 				}
 			}
-			return false;
+		}
+		
+		public InputStream downloadProject(File dir, Date date) throws IOException {
+			URL url = new URL(NotasConstants.DOWNLOAD_ZIPED_PROJECT);
+			Log.e("NotasHandler", "Downloading zip from " + url.toString());
+			
+			URLConnection ucon = url.openConnection();
+			InputStream is = ucon.getInputStream();
+			BufferedInputStream bis = new BufferedInputStream(is);
+			ByteArrayBuffer baf = new ByteArrayBuffer(5000);
+			int current = 0;
+			while ((current = bis.read()) != -1) {
+				baf.append((byte) current);
+			}
+			
+			File file = new File(dir, "dfusion.zip");
+			Log.e("NotasHandler", "Writing downloaded zip to " + file.getAbsolutePath());
+			
+			FileOutputStream fos = new FileOutputStream(file); 
+			fos.write(baf.toByteArray());
+			fos.flush();
+			fos.close();
+			
+			String dateString = getDateFormat().format(date);			
+			Log.e("NotasHandler", "Setting lastDownload key to " + dateString);
+			
+			getSharedPreferences().edit().putString(NotasConstants.ULTIMA_NOTA_BAJADA, dateString).commit();
+			
+			return new FileInputStream(file);
+		}
+		
+		private void emptyDirectory(File dir){
+			String[] children = dir.list();
+	        for (int i = 0; i < children.length; i++) {
+	            File file = new File(dir, children[i]);
+	            boolean success = file.delete();
+	            if(success){
+	            	Log.d("EducARActivity", "Deleted " + file.getPath());
+	            } else {
+	            	Log.d("EducARActivity", "Couldn't delete " + file.getPath());
+	            }
+	        }
 		}
 
+		private void extractZip(InputStream in){
+			ZipInputStream zis = null;
+			try {
+				zis = new ZipInputStream(new BufferedInputStream(in));
+				
+				ZipEntry ze;
+				while ((ze = zis.getNextEntry()) != null) {
+					if(ze.isDirectory()){
+						File file = new File(getUnzipDir(), ze.getName());
+						file.mkdirs();
+					} else {
+						File file = new File(getUnzipDir(), ze.getName());
+						
+						OutputStream out = null;
+						try {
+							out = new BufferedOutputStream(new FileOutputStream(file), 1024);
+							byte[] buffer = new byte[1024];
+							int count;
+							while ((count = zis.read(buffer)) != -1) {
+								out.write(buffer, 0, count);
+							}
+						} catch (IOException e){
+							Log.d("EducARActivity", e.getMessage());
+							error = true;
+						} finally {
+							if(out != null){
+								try {
+									out.flush();
+									out.close();
+								} catch (IOException e){ }
+							}
+						}
+					}
+				}
+			} catch (IOException e){
+				Log.d("EducARActivity", e.getMessage());
+				error = true;
+			} finally {
+				if(zis != null){
+					try {
+						zis.close();
+					} catch (IOException e) { }
+				}
+			}
+		}
 		
 	
 		@Override
@@ -281,7 +340,8 @@ public class EducARActivity extends Activity {
 				       });
 				builder.create().show();
 			} else {
-				_tiComponent.loadScenario(getProjectFile().getPath());
+				File file = new File(getUnzipDir(), getProjectFilename());
+				_tiComponent.loadScenario(file.getPath());
 	    	    _tiComponent.playScenario();
 			}
 		}
