23cab8a7065575a56bd4a3777f891ff500413459
Martin Dutra <martindsq@hotmail.com>
2013-02-23 15:13:32 -0200

    campo magnetico

---
+++ campo-magnetico-android/src/com/educar/giroscopo/notas/constants/NotasConstants.java
@@ -0,0 +1,12 @@
+package com.educar.giroscopo.notas.constants;
+
+public class NotasConstants {
+	
+	public static final String ULTIMA_NOTA_BAJADA          = "UltimaNotaBajada";
+	//public static final String ULTIMAS_NOTAS_URL 			= "/notas-backend/nota/pro"
+	//public static final String ULTIMAS_NOTAS_URL           = "http://192.168.1.103/notas-backend/index.php/nota/ultimasNotas/";
+	//public static final String GET_NOTA_URL                = "http://192.168.1.103/notas-backend/index.php/nota/getNota/";
+	public static final String PROJECT_LAST_MODIFIED       = "http://192.168.1.5/notas-backend/nota/projectLastModified/";
+	public static final String DOWNLOAD_ZIPED_PROJECT      = "http://192.168.1.5/notas-backend/nota/downloadZippedProject/";
+		
+}
